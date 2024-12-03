aad055f1c4238a81d1a04f65b943dfa81483f041
Eldar <elbro.ya@mail.ru>
2024-12-03 18:46:38 +0300

    init

---
+++ src/main/java/com/eldar/ISlab1/ISlab1Application.java
@@ -0,0 +1,13 @@
+package com.eldar.ISlab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISlab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISlab1Application.class, args);
+	}
+
+}
